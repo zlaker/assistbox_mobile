cfdffb52d7c23e848d30a5e4afd8be6716bb5e9e
Alex Ruliov <al.ruliov@gmail.com>
2025-11-29 22:27:52 +0300

    Disable user registration, update dependencies, and ignore deploy_remote.sh

--- config/fortify.php
+++ config/fortify.php
@@ -144,7 +144,8 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Отключаем регистрацию новых пользователей
+        // Features::registration(),
         Features::resetPasswords(),
         Features::emailVerification(),
         Features::twoFactorAuthentication([
