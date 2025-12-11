52d824f1e50ffa669f29c20132adba270e57a191
Alex Ruliov <al.ruliov@gmail.com>
2025-12-11 13:32:01 +0300

    feat: Add Yandex.Metrika counter to landing and layout

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -81,6 +81,20 @@
         <link href="{{ asset('landing/assets/libs/@mdi/font/css/materialdesignicons.min.css') }}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ asset('landing/assets/css/tailwind.css') }}">
 
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105790963', 'ym');
+
+            ym(105790963, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/105790963" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+
     </head>
     
     <body class="font-nunito text-base text-slate-900 dark:text-white dark:bg-slate-900">
